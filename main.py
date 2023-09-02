652579330b7fccb61ec7e4374c8c6d957d253cc3
zehirev-vasil <zehirev.vasil@gmail.com>
2023-09-02 21:53:26 +0300

    REBASE

--- main.py
+++ main.py
@@ -13,6 +13,6 @@ import Loops
 
 # Press the green button in the gutter to run the script.
 if __name__ == "__main__":
-    pass
+    Loops.my_function()
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
