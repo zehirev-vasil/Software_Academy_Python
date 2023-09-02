66914af868c3cae706fcb68c99e62288600cd92e
zehirev-vasil <zehirev.vasil@gmail.com>
2023-09-02 21:53:26 +0300

    LATEST_CHANGES

---
+++ main.py
@@ -0,0 +1,18 @@
+# This is a sample Python script.
+
+# Press Ctrl+Enter to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+# def print_hi(name):
+#     # Use a breakpoint in the code line below to debug your script.
+#     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+#
+
+import Loops
+
+# Press the green button in the gutter to run the script.
+if __name__ == "__main__":
+    Loops.my_function()
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
